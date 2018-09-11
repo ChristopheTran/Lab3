3d8651746c5068ec7c486a41c987259e3705f8d9
unknown <christophetran01@gmail.com>
2018-09-11 16:15:46 -0400

    change to main

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,22 +1,26 @@
 import java.util.ArrayList;
 
 public class AddressBook {
-	private ArrayList<BuddyInfo> buddies;
+	private static ArrayList<BuddyInfo> buddies;
 	
 	public AddressBook() {
-		this.buddies=null;
+		buddies = new ArrayList<BuddyInfo>();
 	}
 
-	public void addBuddies(BuddyInfo n) {
-		this.buddies.add(n);
+	public static void addBuddies(BuddyInfo n) {
+		buddies.add(n);
 		return;
 	}
-	public void removeBuddies(BuddyInfo n) {
-		this.buddies.remove(n);
+	public static void removeBuddies(BuddyInfo n) {
+		buddies.remove(n);
 		return;
 	}
 	
 	public static void main(String[] args) {
+		BuddyInfo buddy = new BuddyInfo("Bob","112 street", "911");
+		AddressBook book = new AddressBook();
+		book.addBuddies(buddy);
+		book.removeBuddies(buddy);
 		System.out.println("Address Book");
 
 	}
